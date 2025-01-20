9fe37188fdac70552e78c2b72681dc2c91728212
MarceloJr1 <sampaiomarcelo981@gmail.com>
2025-01-20 18:21:16 -0300

    debug

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,3 +1,7 @@
 alert("bem vindo ao jogo do numero secreto!");
 let numerosecreto = 29;
-prompot("escolha um número de 1 a 30");
+let chute = prompt("escolha um número de 1 a 30");
+
+if(numerosecreto == chute) {
+    console.log("isso aí! você é muito esperto.");
+}
