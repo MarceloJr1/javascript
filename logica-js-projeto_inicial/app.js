51428d7488ea6b407d2798ced6069a6fa817d709
MarceloJr1 <sampaiomarcelo981@gmail.com>
2025-01-20 18:22:25 -0300

    debug2

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -4,4 +4,6 @@ let chute = prompt("escolha um número de 1 a 30");
 
 if(numerosecreto == chute) {
     console.log("isso aí! você é muito esperto.");
+} else {
+    console.log("Número errado :(");
 }
