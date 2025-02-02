1e46b8e0aa99c36154267af1b8421a9916630ce6
MarceloJr1 <sampaiomarcelo981@gmail.com>
2025-02-02 10:48:43 -0300

    Complete1

---
+++ modulo 02/app.js
@@ -0,0 +1,9 @@
+let paragrafo = document.querySelector("p");
+let titulo = document.querySelector("h1");
+
+paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+titulo.innerHTML = "Jogo do Número Secreto";
+
+function verificarChute() { 
+    console.log("o botão foi clicado.")
+}
