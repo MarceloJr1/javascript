2a1350401a44ed4b4144a55a08a7d5a54f338d62
MarceloJr1 <sampaiomarcelo981@gmail.com>
2025-02-27 20:12:02 -0300

    update game

--- modulo 02/app.js
+++ modulo 02/app.js
@@ -1,9 +1,17 @@
 let paragrafo = document.querySelector("p");
 let titulo = document.querySelector("h1");
+let NumeroSecreto = gerarNumeroAleatório();
 
 paragrafo.innerHTML = "Escolha um número entre 1 e 10";
 titulo.innerHTML = "Jogo do Número Secreto";
 
 function verificarChute() { 
-    console.log("o botão foi clicado.")
+    let chute = document.querySelector("input").value;
+    console.log(chute == NumeroSecreto);
+}
+
+
+
+function gerarNumeroAleatório() {
+    return parseInt(Math.random() * 10 + 1 );
 }
