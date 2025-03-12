db01c0e17454e9015c3a88b93919960fd4f4725c
MarceloJr1 <sampaiomarcelo981@gmail.com>
2025-03-12 16:56:26 -0300

    Update game

--- modulo 02/app.js
+++ modulo 02/app.js
@@ -5,13 +5,25 @@ let NumeroSecreto = gerarNumeroAleatório();
 paragrafo.innerHTML = "Escolha um número entre 1 e 10";
 titulo.innerHTML = "Jogo do Número Secreto";
 
-function verificarChute() { 
-    let chute = document.querySelector("input").value;
-    console.log(chute == NumeroSecreto);
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }
 }
 
 
 
 function gerarNumeroAleatório() {
     return parseInt(Math.random() * 10 + 1 );
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute = '';
 }
