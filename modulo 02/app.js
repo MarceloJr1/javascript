5a55f8455f27be4dc0dd4798f371bc74869df3c7
MarceloJr1 <sampaiomarcelo981@gmail.com>
2025-03-14 20:56:40 -0300

    organization

--- modulo 02/app.js
+++ modulo 02/app.js
@@ -1,29 +1,46 @@
+
+// variaveis
 let paragrafo = document.querySelector("p");
 let titulo = document.querySelector("h1");
-let NumeroSecreto = gerarNumeroAleatório();
+let NumeroSecreto = gerarNumeroAleatório;
+let chance = 1;
 
+// corpo do código
 paragrafo.innerHTML = "Escolha um número entre 1 e 10";
 titulo.innerHTML = "Jogo do Número Secreto";
 
-function verificarChute() {
-    let chute = document.querySelector('input').value;
-    
-    if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Acertou!');
-        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
-        exibirTextoNaTela('p', mensagemTentativas);
-        document.getElementById('reiniciar').removeAttribute('disabled');
-    }
+    //Funcções
+function exibirNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
-
-
-
 function gerarNumeroAleatório() {
     return parseInt(Math.random() * 10 + 1 );
 }
 
 function limparCampo(){
-    chute = document.querySelector('input');
-    chute = '';
+    chute = document.querySelector("input");
+    chute = "";
 }
+
+function verificarChute() {
+    let chute = document.querySelector("input").value;
+    
+    if (chute == NumeroSecreto) {
+        exibirNaTela("h1", "Acertou!");
+        let palavraTentativa = chance > 1 ? "tentativas" : "tentativa";
+        let mensagemTentativas = "Você descobriu o número secreto com ${chance} ${palavraTentativa}!";
+        exibirNaTela("p", mensagemTentativas);
+        document.getElementById("reiniciar").removeAttribute("disabled");
+    }
+    else {
+        if (chute > NumeroSecreto) {
+            exibirNaTela("p", "O número secreto é menor");
+        } else{
+            exibirNaTela("p", "O número secreto é menor");
+        }
+    }
+}
+
+
+
